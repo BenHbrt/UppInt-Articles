49f51ba1f90190643c14a38bafa79f7f929561c5
BenHbrt <benherbert1983@googlemail.com>
2023-01-19 18:16:25 +0100

    Questions1

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,15 +1,7 @@
 export const questionList = [
-    "Fernando is a burrachito!",
-    // "There are many Ø people around the world who like the president and his Ø pets.",
-    "I want a new car.",
-    // "I can see an apple.",
-    // "I know the answer.",
-    // "I think Ø mice are great Ø animals.",
-    // "The woman is friends with a doctor.",
-    // "Some people know the answer to the question.",
-    // "ØØ people like money.",
-    // "There are many Ø people around the world who like the president and his Ø pets.",
-    // "I like Ø coffee."
+    "When I was walking home, I saw a man. The man was talking on his mobile phone.",
+    "The British Council is a good place to learn Ø English.",
+    "I can't come to the meeting right now because I'm not at Ø work."
 ]
 
 export const questionListLength = questionList.length
