3b918ee307a0822fdaa6b34b6c6e2a9193e96acc
BenHbrt <benherbert1983@googlemail.com>
2023-01-20 15:04:56 +0100

    Questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,7 +1,24 @@
 export const questionList = [
     "When I was walking home, I saw a man. The man was talking on his mobile phone.",
-    "The British Council is a good place to learn Ø English.",
-    "I can't come to the meeting right now because I'm not at Ø work."
+    "The river Vltava runs through the centre of Ø Prague.",
+    "The only problem I have with this solution is that it's too expensive.",
+    "Ø British drivers are used to driving on the left-hand side of the road.",
+    "Many people like to go skiing in the Alps.",
+    "For this recipe you will need: an aubergine, Ø rice, and the juice of one lemon.",
+    "The University of Brighton is in the UK, whereas Ø Charles Univerity is in the Czech Republic.",
+    "I can't come to the meeting right now because I'm not at Ø work.",
+    "ØØ Sněžka is the highest point in the Czech Republic.",
+    "The Maldives are located in the Indian Ocean.",
+    "ØØ cats are commonly thought to be Ø clean creatures.",
+    "If you want to live in the Middle East, it helps if you can tolerate Ø hot weather.",
+    "I don't like going to Ø large, showy weddings. I'd only go to a wedding if it was very low-key.",
+    "'ØØ money makes the world go round.'",
+    "After she'd had an accident, she was taken to the city hospital.",
+    "ØØ Louvre is a famouse art gallery in Ø Paris.",
+    "The first time he did it, I was annoyed. However, the second time he did it, I was furious!",
+    "I usually like Ø snakes, but the snakes I saw on my trip to the jungle were huge and terrifying!",
+    "The metro station near my flat is now closed, so I have travel to work by Ø tram instead.",
+    "The British Council is a good place to learn Ø English."
 ]
 
 export const questionListLength = questionList.length
